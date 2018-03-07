ff35d928a6d1cadc1aa452a67ff997d8c67b60e1
HasithaAbeysekera <hasithaa@hotmail.com>
2018-03-07 20:12:24 +0800

    renamed to -clap, added -lolice

---
+++ commands/clap.js
@@ -0,0 +1,15 @@
+const Discord = require("discord.js");
+var assets = require('../assets/assets.json');
+
+exports.run = function(client, message, args) {
+
+  const embed = new Discord.RichEmbed()
+      .setImage(assets["puckclap"])
+      .setColor(0xFFFFFF)
+  message.channel.send(embed);
+};
+
+exports.help = {
+  name: 'clap',
+  description: 'Puck approves'
+};
--- commands/lolice.js
+++ commands/lolice.js
@@ -4,12 +4,12 @@ var assets = require('../assets/assets.json');
 exports.run = function(client, message, args) {
 
   const embed = new Discord.RichEmbed()
-      .setImage(assets["puckclap"])
+      .setImage(assets["lolice"])
       .setColor(0xFFFFFF)
   message.channel.send(embed);
 };
 
 exports.help = {
-  name: 'puckclap',
-  description: 'Puck approves'
+  name: 'lolice',
+  description: 'Someone call the Lolice'
 };
